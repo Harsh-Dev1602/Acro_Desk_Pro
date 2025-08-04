a66c47cc7c208ad06af9853d871efdfb975a586e
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-08-04 22:12:27 +0530

    AcroDesk Pro-[#250804]

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,17 +10,19 @@ import About from './Com/About'
 function App() {
   return (
     <>
-    <div className="w-[94%] h-auto mx-auto  min-[1700px]:w-[70%] min-[3500px]:w-[20%] bg-white text-black">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contect" element={<Contect/>} />
-      </Routes>
-      <Footer />
-    </div>
+      <div className="bg-white text-black">
+        <div className="w-[94%] h-auto mx-auto  min-[1700px]:w-[70%] min-[3500px]:w-[20%] ">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contect" element={<Contect />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
