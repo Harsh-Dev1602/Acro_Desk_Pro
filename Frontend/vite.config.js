5297985e11b89f419b06d366a60b7d2b216bcda2
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-08-17 22:12:38 +0530

    AcroDesk Pro-[#250817]

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3002,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://acro-desk-pro-up2l.vercel.app/",
         changeOrigin: true,
       },
     },
